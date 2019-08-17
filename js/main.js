53671360dbb31c6a4e26b0d7e6d4d89a8a4743c5
2kindsofcs <42531877+2kindsofcs@users.noreply.github.com>
2019-08-17 23:01:46 +0900

    Revert code not to add another title tag

--- js/main.js
+++ js/main.js
@@ -66,12 +66,7 @@ var main = {
 
     // show the big header image
     main.initImgs();
-    // Add title to head tag
-    firstHeadText = document.getElementsByTagName("h2")[0].innerText;
-    headTag = document.getElementsByTagName('head')[0];
-    titleTag = document.createElement("title");
-    titleTag.innerText = firstHeadText;
-    headTag.appendChild(titleTag);
+
   },
 
   initImgs : function() {
